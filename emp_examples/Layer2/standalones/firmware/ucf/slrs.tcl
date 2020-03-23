5c2e854d466d2311bf371249488370ac874bf8b0
Sioni Summers <sioni.summers10@imperial.ac.uk>
2020-03-23 21:29:22 +0100

    A little closer: WNS:-0.030ns, TNS:-2.099ns. Vivado could have tried harder tbh.

--- emp_examples/Layer2/standalones/firmware/ucf/slrs.tcl
+++ emp_examples/Layer2/standalones/firmware/ucf/slrs.tcl
@@ -1,5 +1,5 @@
 set_property USER_SLR_ASSIGNMENT SLR0 [get_cells payload/Merge/Merge/MergeLayer1[0].merge]
-set_property USER_SLR_ASSIGNMENT SLR1 [get_cells payload/Merge/Merge/MergeLayer1[1].merge]
+set_property USER_SLR_ASSIGNMENT SLR2 [get_cells payload/Merge/Merge/MergeLayer1[1].merge]
 set_property USER_SLR_ASSIGNMENT SLR2 [get_cells payload/Merge/Merge/MergeLayer1[2].merge]
 
 set_property USER_SLR_ASSIGNMENT SLR1 [get_cells payload/Merge/Merge/MergeLayer2]
